9c7eabc4e59e7ae4420501b217a34e36d14d29f9
Swastik Ranjan Panigrahi <swastikrpanigrahi@gmail.com>
2024-09-11 07:34:15 +0530

    initialising project

--- script.js
+++ script.js
@@ -7,10 +7,20 @@ fs.readFile("hello.txt","utf8", function(err, data){
     else console.log(data);
 })
 
-*/
+
 
 const server=http.createServer(function(req,res){
     res.end("HELLO WORLD");
 })
 server.listen(3000);
+*/
 
+
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello Paaji')
+})
+
+app.listen(3000);
