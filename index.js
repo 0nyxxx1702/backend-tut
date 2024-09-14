fc0ad5b1e036afc4689e7a462f440a6394d70ee8
Swastik Ranjan Panigrahi <swastikrpanigrahi@gmail.com>
2024-09-14 09:17:45 +0530

    done the task maker webapp

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const path=require('path');
 const express=require('express');
 const app=express();
+const fs=require('fs');
 
 
 app.use(express.json());
@@ -9,8 +10,23 @@ app.use(express.static(path.join(__dirname,'public')))
 app.set('view engine', 'ejs');
 
 app.get("/", function(req, res){
-    res.render("index");
+    fs.readdir(`./files`, function(err, files){
+        res.render("index", {files: files}) ;
+    })  
 })
+app.get('/file/:filename', function(req, res){
+    fs.readFile(`./files/${req.params.filename}`, "utf-8", function(err, filedata){
+        res.render('show', {filename: req.params.filename, filedata: filedata })
+    })
+})
+app.post("/create", function(req, res){
+   fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function(err){
+    res.redirect("/");
+   }) 
+})
+
+
+
 app.get("/profile/:username", function(req, res){
     res.send(`welcome, ${req.params.username}`);
 })
